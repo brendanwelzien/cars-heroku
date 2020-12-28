5503a8eb24ee1b714b81a5d03239796df1936f88
Brendan Welzien <brendanwelzien@gmail.com>
2020-12-28 15:21:07 -0800

    urls

--- cars/urls.py
+++ cars/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import CarList, CarDetail
-# urls
+# url notes
 urlpatterns = [
     path('', CarList.as_view(), name='car-list'),
     path('<int:pk>/', CarDetail.as_view(), name='car-detail'),
